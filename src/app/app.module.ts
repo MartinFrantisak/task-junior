af325d11fb9b602e9dc3606464d1558ff8e66f17
Martin Frantisak <martin.frantisak@gmail.com>
2022-06-29 23:14:43 +0200

    Added customer-overview and other-view components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CustomerOverviewComponent } from './components/customer-overview/customer-overview.component';
+import { OtherViewsComponent } from './components/other-views/other-views.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    CustomerOverviewComponent,
+    OtherViewsComponent
   ],
   imports: [
     BrowserModule,
