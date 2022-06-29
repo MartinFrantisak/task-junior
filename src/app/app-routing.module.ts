221219dc9b14b6ed43c3aa084306fb02f2a20108
Martin Frantisak <martin.frantisak@gmail.com>
2022-06-29 23:16:00 +0200

    Created navigation between customer-overview and other-views components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomerOverviewComponent} from "./components/customer-overview/customer-overview.component";
+import {OtherViewsComponent} from "./components/other-views/other-views.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'customer-overview', component: CustomerOverviewComponent},
+  {path: 'reports', component: OtherViewsComponent, data: {title: 'Reports'}},
+  {path: 'file-transcoding', component: OtherViewsComponent, data: {title: 'File Transcoding'}},
+  {path: 'notification-center', component: OtherViewsComponent, data: {title: 'Notification Center'}}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
