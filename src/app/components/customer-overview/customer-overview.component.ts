626a325984d54744d4d16f3f24d2427c959bc08e
Martin Frantisak <martin.frantisak@gmail.com>
2022-07-02 14:55:11 +0200

    Added function to sort customers by size

--- src/app/components/customer-overview/customer-overview.component.ts
+++ src/app/components/customer-overview/customer-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-customer-overview',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerOverviewComponent implements OnInit {
 
-  constructor() { }
+  customers: any = []
+  sortBySize(): void {
+    this.customers.sort(function(a: { size: any; }, b: { size: any; }) {
+      a = a.size.toUpperCase().slice(0, -3)
+      b = b.size.toUpperCase().slice(0, -3)
+      return Number(b) - Number(a)
+    })
+    console.log(this.customers)
+  }
 
+  search(): void {
+    let c = document.getElementById("searchData")
+    console.log(c)
+  }
+
+  constructor(private httpClient: HttpClient) {
+  }
   ngOnInit(): void {
+    this.httpClient.get("assets/customerData.json").subscribe(data => {
+      this.customers = data
+    })
   }
 
 }
